d058bb6aa86b1f617e67964461b4c363b9943b5c
John Bramley <bramleyjl@gmail.com>
2025-12-28 11:01:12 -0700

    Fix whitespace in footer

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -195,7 +195,7 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
+  footer: `Made with <a
       class="text-primary" href="https://github.com/arifszn/gitprofile"
       target="_blank"
       rel="noreferrer"
